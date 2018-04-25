ce2b6ff0dda02485475b0cea74046210f28cd79d
Sam Slutzky <samuels@microsoft.com>
2018-04-25 09:46:05 -0700

    Mdoule 2-2

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,11 @@
+﻿namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+        StandardGradeBook(string name)
+            : base(name)
+        {
+            Type = Enums.GradeBookType.Standard;
+        }
+    }
+}
