7429d55888dfcd7ab04ab20bac64e15e2e5e34e5
Sam Slutzky <samuels@microsoft.com>
2018-04-25 10:42:43 -0700

    Assign ranked grades

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -10,6 +10,21 @@ namespace GradeBook.GradeBooks
             : base(name)
         {
             Type = Enums.GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("At least five students are required for ranked grading.");
+            }
+
+            List<double> sortedAverageGrades = Students.Select(s => s.AverageGrade).ToList();
+            sortedAverageGrades.OrderByDescending(g => g);
+
+            int studentIndex = sortedAverageGrades.FindIndex(g => g == averageGrade);
+            char grade = (char)((int)'A' + (studentIndex / (Students.Count / 5)));
+            return (studentIndex == -1 || grade == 'E') ? 'F' : grade;
         }
     }
 }
